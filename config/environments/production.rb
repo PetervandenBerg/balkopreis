fb08a0c4e4d5934971cb0abb2b0a023d0a9c199e
Peter van den Berg <Peter_vd_berg@hotmail.com>
2014-03-04 12:53:24 +0100

    asset changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Teampage::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.initialize_on_precompile = false
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
