426e93e3bf21073946bc8a2584203a27aefc91a2
Peter van den Berg <Peter_vd_berg@hotmail.com>
2014-05-26 21:12:38 +0200

    minor changes to preparations

--- app/controllers/preparations_controller.rb
+++ app/controllers/preparations_controller.rb
@@ -1,22 +1,38 @@
 class PreparationsController < ApplicationController
 
   def index
-    @preparations = Preparation.all
+    @preparations = Preparation.all.order('created_at DESC')
   end
 
   def new
     @preparation = Preparation.new
   end 
 
+  def edit
+    @preparation = Preparation.find(params[:id])
+  end
+
   def create
     @preparation = Preparation.new(preparation_params)
 
     respond_to do |format|
       if @preparation.save
-        format.html { redirect_to preparations_path, notice: 'Prepration was successfully created.' }
+        format.html { redirect_to preparations_path, notice: 'Voorbereiding was successfully created.' }
         format.json { render action: 'show', status: :created, location: @preparation }
       else
         format.html { render action: 'new' }
+        format.json { render json: @preparation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @preparation.update(preparation_params)
+        format.html { redirect_to preparations_path, notice: 'Voorbereiding was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
         format.json { render json: @preparation.errors, status: :unprocessable_entity }
       end
     end
